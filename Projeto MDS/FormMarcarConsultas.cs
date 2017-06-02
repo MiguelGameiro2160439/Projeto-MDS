15e5e0b8e9f88e22d4221e17dbae065f0057507f
João Vieira <Joo Vieira@DESKTOP-V1NPST5>
2017-06-02 20:34:22 +0100

    mais coisas feitas, nao vou conseguir acabar tudo. tenho de irembora as 22:00, vou fazer o que posso, nem que seja codigo sem testar, voces se tiverem mais alguma coisa a fazer, desenrasquem se.

--- Projeto MDS/FormMarcarConsultas.cs
+++ Projeto MDS/FormMarcarConsultas.cs
@@ -12,18 +12,66 @@ namespace Projeto_MDS
 {
     public partial class FormMarcarConsultas : Form
     {
-        public FormMarcarConsultas()
+        private Model1Container container;
+        public Consulta NovaConsula { get; private set; }
+
+        public FormMarcarConsultas(Model1Container container)
         {
             InitializeComponent();
+
+            this.container = new Model1Container();
+
+            if (container != null)
+            {
+               comboBoxPaciente.Items.AddRange(container.PacienteSet.ToArray());
+
+            }
+
+            refreshListaPacientes();
+
         }
 
         private void buttonRegistarPaciente_Click(object sender, EventArgs e)
         {
-
+            FormRegistarPaciente form = new FormRegistarPaciente();
+            DialogResult resultado = form.ShowDialog();
+            this.Close();
+            if (resultado == DialogResult.OK)
+            {
+                AdicionarPaciente(form.NovoPaciente);
+            }
         }
+
+        private void refreshListaPacientes()
+        {
+            comboBoxPaciente.Items.Clear();
+            comboBoxPaciente.Items.AddRange(container.PacienteSet.ToArray());
+        }
+
+        private void AdicionarPaciente(Paciente paciente)
+        {
+            container.PacienteSet.Add(paciente);
+            container.SaveChanges();
+        }
+
 
         private void buttonMarcarConsulta_Click(object sender, EventArgs e)
         {
+            Paciente paciente = null;
+
+            if (comboBoxPaciente.SelectedIndex > -1)
+            {
+                paciente = (Paciente)comboBoxPaciente.SelectedItem;
+            }
+
+            dateTimePickerConsulta.Format = DateTimePickerFormat.Custom;
+            dateTimePickerConsulta.CustomFormat = "MM/dd/yyyy hh:mm:ss";
+
+            NovaConsula = new Consulta();
+            
+            NovaConsula.Data = dateTimePickerConsulta.Value;
+            NovaConsula.Descricao = "";
+            NovaConsula.Paciente = paciente;       
 
         }
     }
---
+++ Projeto MDS/FormTabelaPaciente.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_MDS
+{
+    public partial class FormTabelaPaciente : Form
+    {
+        private Model1Container container;
+        private Paciente pacienteSelecionado;
+
+        public FormTabelaPaciente()
+        {
+            InitializeComponent();
+
+            container = new Model1Container();
+
+            refreshListaPacientes();
+        }
+
+        private void buttonInserir_Click(object sender, EventArgs e)
+        {
+            FormRegistarPaciente form = new FormRegistarPaciente();
+            DialogResult resultado = form.ShowDialog();
+
+            if (resultado == DialogResult.OK)
+            {
+                AdicionarPaciente(form.NovoPaciente);
+            }
+
+        }
+
+
+        private void refreshListaPacientes()
+        {
+            listBoxPacientes.Items.Clear();
+            listBoxPacientes.Items.AddRange(container.PacienteSet.ToArray());
+        }
+        private void AdicionarPaciente(Paciente paciente)
+        {
+    
+                container.PacienteSet.Add(paciente);
+                container.SaveChanges();
+                refreshListaPacientes();
+   
+        }
+
+        private void listBoxPacientes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            pacienteSelecionado = (Paciente)listBoxPacientes.SelectedItem;
+        }
+
+        private void buttonEditar_Click(object sender, EventArgs e)
+        {
+            pacienteSelecionado = (Paciente)listBoxPacientes.SelectedItem;
+
+            if (pacienteSelecionado != null)
+            {
+                //remover
+                pacienteSelecionado = (Paciente)listBoxPacientes.SelectedItem;
+
+                container.PacienteSet.Remove(pacienteSelecionado);
+
+                container.SaveChanges();
+
+
+                listBoxPacientes.Items.AddRange(container.PacienteSet.ToArray());
+
+                //insere
+                FormRegistarPaciente form = new FormRegistarPaciente();
+                DialogResult resultado = form.ShowDialog();
+
+                if (resultado == DialogResult.OK)
+                {
+                    AdicionarPaciente(form.NovoPaciente);
+                }
+
+            }
+        }
+
+        private void buttonRemover_Click(object sender, EventArgs e)
+        {
+            pacienteSelecionado = (Paciente)listBoxPacientes.SelectedItem;
+
+            container.PacienteSet.Remove(pacienteSelecionado);
+
+            container.SaveChanges();
+
+
+            listBoxPacientes.Items.AddRange(container.PacienteSet.ToArray());
+        }
+    }
+}
